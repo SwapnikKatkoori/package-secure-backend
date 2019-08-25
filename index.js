f9c1ecc396da7b04ebcb26144accf8f431c71ef8
Swapnik Katkoori <katkoor2@msu.edu>
2019-08-25 09:25:44 -0400

    added new trial

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const io = require('socket.io')(server);
 
 var database = [{account_sid: 'AC69545bf97209c17cacc09c7b58c89133', auth_token: '66d717737dea99ffdadaef94c114120d', number: '+15175283678', trial: "+19523146862"},
 				{account_sid: 'AC559e873ef9a5629f721e874a3b455b8e', auth_token: '0158cc48c2af684020d663b730d49245', number: '+15172432773', trial: "+13343198204"},
-				{account_sid: 'AC2e03af82eda22dfcc9960e284e5fa74c', auth_token: 'd622bf5ea638178d6bb18b4afe93f7db', number: '+12269720492', trial: "+12267807021"}]
+				{account_sid: 'AC2e03af82eda22dfcc9960e284e5fa74c', auth_token: 'd622bf5ea638178d6bb18b4afe93f7db', number: '+12269720492', trial: "+12267807021"},
+				{account_sid: 'ACff988cd0babf2472d47d9f553a2a277e', auth_token: 'c168f49f002a3ae090babb3a08375617', number: '+16478523655', trial: '+12264997765'}]
 
 io.on('connection', client => {
 
@@ -23,9 +24,10 @@ io.on('connection', client => {
 
   client.on('deny', data => { 
   	var twilioClient = new twilio('AC69545bf97209c17cacc09c7b58c89133', '66d717737dea99ffdadaef94c114120d');
+  	var main_user = database[0]
   	twilioClient.messages.create({
-	  to: '+15175283678',
-	  from: '+19523146862',
+	  to: main_user.number,
+	  from: main_user.trial,
 	  body: 'Your package has been stolen. A report has been sent to Amazon.'
 	});
 
