53dc12674452a4d4f65cd905339e3ac645e2e570
Swapnik Katkoori <katkoor2@msu.edu>
2019-08-25 09:02:42 -0400

    changes

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const io = require('socket.io')(server);
 
 var database = [{account_sid: 'AC69545bf97209c17cacc09c7b58c89133', auth_token: '66d717737dea99ffdadaef94c114120d', number: '+15175283678', trial: "+19523146862"},
 				{account_sid: 'AC559e873ef9a5629f721e874a3b455b8e', auth_token: '0158cc48c2af684020d663b730d49245', number: '+15172432773', trial: "+13343198204"},
-				{account_sid: 'AC2e03af82eda22dfcc9960e284e5fa74c', auth_token: 'd622bf5ea638178d6bb18b4afe93f7db', number: '+12269720492', trial: "place_holder"}]
+				{account_sid: 'AC2e03af82eda22dfcc9960e284e5fa74c', auth_token: 'd622bf5ea638178d6bb18b4afe93f7db', number: '+12269720492', trial: "+12267807021"}]
 
 io.on('connection', client => {
 
   client.on('confirm', data => {
 	console.log("Package was confirmed")
-
-	var twilioClient = new twilio('AC69545bf97209c17cacc09c7b58c89133', '66d717737dea99ffdadaef94c114120d');
+	var main_user = database[0]
+	var twilioClient = new twilio(main_user.account_sid, main_user.auth_token);
 
 	// Send the text message.
 	twilioClient.messages.create({
-	  to: '+15175283678',
-	  from: '+19523146862',
+	  to: main_user.number,
+	  from: main_user.trial,
 	  body: 'Thank you for confirming!'
 	});
   });
